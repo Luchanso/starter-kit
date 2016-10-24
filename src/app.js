c55022cbf04960b7851ab0b4dc532f43cca33344
Luchanso <luchansodev@gmail.com>
2016-10-24 05:32:03 +0300

    Some fix

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ window.onresize = () => {
   Engine.game.scale.setGameSize(window.innerWidth, window.innerHeight)
 }
 
-Engine.game.state.add('Boot', Engine.Boot)
-Engine.game.state.add('Game', Engine.Game)
-Engine.game.state.add('Loader', Engine.Loader)
+Engine.game.state.add('Boot', Boot)
+Engine.game.state.add('Game', Game)
+Engine.game.state.add('Loader', Loader)
 
 Engine.game.state.start('Boot')
