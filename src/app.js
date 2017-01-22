1392a97c57359e499bea4e850fc870c2a6c927f9
Luchanso <luchansodev@gmail.com>
2017-01-22 15:24:37 +0300

    put apart analytics

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
+import * as analytics from './vendors/analytics';
+
+analytics.init();
+
 Engine.game = new Phaser.Game(Engine.maxWidth, Engine.maxHeight, Phaser.AUTO);
 
 window.onresize = () => {
   Engine.game.scale.setGameSize(window.innerWidth, window.innerHeight);
 }
 
-let a = { b: 1234 };
+let a = { b: 123456 };
 let { b } = a;
 
 Engine.game.state.add('Boot', Boot);
