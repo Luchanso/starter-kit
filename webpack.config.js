8d1b18bfff04eb9ea271a3e83c8836db77ced99b
Luchanso <luchansodev@gmail.com>
2017-01-22 15:22:57 +0300

    add hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,44 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+const env = process.env.NODE_ENV || 'development';
+
+let plugins = [
+  new HtmlWebpackPlugin({
+    template: './pages/index.html',
+  }),
+  new CopyWebpackPlugin([
+    {
+      from: './assets',
+      to: './assets',
+    },
+  ]),
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify(env),
+    },
+  }),
+];
+
+if (env === 'development') {
+  plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
+
 module.exports = {
   entry: './src/app.js',
   output: {
     filename: 'bundle.js',
-    path: './dist'
+    path: './dist',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader'
-      }
-    ]
-  }
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins,
 }
